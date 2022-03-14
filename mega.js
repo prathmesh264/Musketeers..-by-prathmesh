1bd5f8f764d76d71f368e19619cc04c3c10335ac
prathmesh264 <99034151+prathmesh264@users.noreply.github.com>
2022-03-14 16:42:21 +0530

    initial commit

---
+++ mega.js
@@ -0,0 +1,15 @@
+let button = document.getElementById('no');
+        button.addEventListener('click', inputMsg)
+        function inputMsg(){
+            let user1 = prompt('create your user name here');
+            let password1 = prompt('create your password here');
+            let password2 = prompt('Rewrite to Confirm password');
+            let Account = alert("SignUp succesfully !!! ")
+
+            function makeAccount(user, password){
+                let Account = alert("your user name is " + use + " and password is " + passwor + " thank you for login in !");
+                return Account;
+            }
+            let login = makeAccount(user1, password1);
+        }
+    
